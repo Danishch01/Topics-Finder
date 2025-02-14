e867994b516504e7b9358dcc9a3c5413decdcb2c
Danishch01 <Danishramzan838@gmail.com>
2025-02-14 07:58:39 +0500

    Update Stories.py

--- Stories.py
+++ Stories.py
@@ -18,13 +18,7 @@ max_results = st.slider("Results per Keyword:", min_value=5, max_value=50, value
 
 # List of broader keywords
 keywords = [
-    "Affair Relationship Stories", "Reddit Update", "Reddit Relationship Advice", "Reddit Relationship",
-    "Reddit Cheating", "AITA Update", "Open Marriage", "Open Relationship", "X BF Caught",
-    "Stories Cheat", "X GF Reddit", "AskReddit Surviving Infidelity", "GurlCan Reddit",
-    "Cheating Story Actually Happened", "Cheating Story Real", "True Cheating Story",
-    "Reddit Cheating Story", "R/Surviving Infidelity", "Surviving Infidelity",
-    "Reddit Marriage", "Wife Cheated I Can't Forgive", "Reddit AP", "Exposed Wife",
-    "Cheat Exposed"
+    "Affair Stories"
 ]
 
 # Fetch Data Button
